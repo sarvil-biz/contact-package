783c41f8cd2793ba50f24156605880f8c3242a27
sarvil <sarvil@pm.biztechconsultancy.com>
2024-04-05 18:18:54 +0530

    View publish

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -12,9 +12,13 @@ class ContactServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views/', 'contact');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
         $this->publishes([
             __DIR__.'/config/contact.php' => config_path('contact.php'),
+            __DIR__.'/views' => resource_path('views/vendor/contact')
         ]);
+
+
     }
 
     public function register()
